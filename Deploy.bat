fb920e51a8c45ba31697c52188ff6099a35e5655
Event <Event@CIT3-MKT-PC3.acg.ac.nz>
2021-08-02 10:44:12 +1200

    deploy

---
+++ Deploy.bat
@@ -0,0 +1,5 @@
+C:\Users\Event\Desktop\Git\bin\git status 
+C:\Users\Event\Desktop\Git\bin\git add * 
+C:\Users\Event\Desktop\Git\bin\git commit -m "deploy" 
+C:\Users\Event\Desktop\Git\bin\git push
+timeout 5 >nul
